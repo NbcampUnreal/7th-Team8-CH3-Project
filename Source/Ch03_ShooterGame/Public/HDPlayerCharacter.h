8b80cf263cdcd83e86758d613326a4ce16d80aeb
kkym323 <kkym32387@gmail.com>
2026-02-12 21:14:55 +0900

    캐릭터 움직이는 방향으로 회전 / 대쉬 구현

--- Source/Ch03_ShooterGame/Public/HDPlayerCharacter.h
+++ Source/Ch03_ShooterGame/Public/HDPlayerCharacter.h
@@ -2,13 +2,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "HDPlayerCharacter.generated.h"
+
 
 class USpringArmComponent;
 class UCameraComponent;
 struct FInputActionValue;
 
 UCLASS()
+
 class CH03_SHOOTERGAME_API AHDPlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
@@ -28,5 +31,7 @@ protected:
 	void Move(const FInputActionValue& value);
 	UFUNCTION()
 	void Dash(const FInputActionValue& value);
-
+	bool bCanDash = true;
+	FTimerHandle DashCooldownTimerHandle;
+	void ResetDash();
 };
