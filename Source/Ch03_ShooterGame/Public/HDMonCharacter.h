aed4f3e3f78d46da88ae976d2fb00d291db78573
MoonIW <miws4747@gmail.com>
2026-02-12 21:10:36 +0900

    몬스터 이동 구현

--- Source/Ch03_ShooterGame/Public/HDMonCharacter.h
+++ Source/Ch03_ShooterGame/Public/HDMonCharacter.h
@@ -14,16 +14,20 @@ class CH03_SHOOTERGAME_API AHDMonCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AHDMonCharacter();
+	// Called every frame.
+	virtual void Tick(float DeltaSeconds) override;
 
+	/** Returns TopDownCameraComponent subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	
+	
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
 
 };
