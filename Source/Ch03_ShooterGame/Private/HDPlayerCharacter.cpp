dde6d8f450bbedc9c247019f396862a28c462d82
MoonIW <miws4747@gmail.com>
2026-02-23 15:59:48 +0900

    캐릭터 데미지및 어택 추가

--- Source/Ch03_ShooterGame/Private/HDPlayerCharacter.cpp
+++ Source/Ch03_ShooterGame/Private/HDPlayerCharacter.cpp
@@ -1,8 +1,12 @@
 ﻿#include "HDPlayerCharacter.h"
 #include "HDPlayerController.h"
+#include "HDBowProjectile.h"
+#include "HDMonCharacter.h"
+#include "HDBowHUD.h"
 #include "EnhancedInputComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include <Kismet/GameplayStatics.h>
 
 AHDPlayerCharacter::AHDPlayerCharacter()
 {
@@ -42,12 +46,15 @@ void AHDPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 		{
 			EnhancedInput->BindAction(
 				PlayerController->MoveAction,ETriggerEvent::Triggered,this,&AHDPlayerCharacter::Move);
+			EnhancedInput->BindAction(
+				PlayerController->AttackAction, ETriggerEvent::Started, this, &AHDPlayerCharacter::Attack);
 		}
 
 		if (AHDPlayerController* PlayerController = Cast<AHDPlayerController>(GetController()))
 		{
 			EnhancedInput->BindAction(PlayerController->DashAction,ETriggerEvent::Triggered,this,&AHDPlayerCharacter::Dash);
 		}
+		
 	}
 }
 
@@ -87,8 +94,76 @@ void AHDPlayerCharacter::Dash(const FInputActionValue& value)
 	}
 }
 
+
 void AHDPlayerCharacter::ResetDash()
 {
 	bCanDash = true;
 	// GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("Dash Ready!"));
+}
+void AHDPlayerCharacter::Fire()
+{
+	
+	if (ProjectileClass)
+	{
+		FVector CharacterLocation = GetActorLocation();
+		FRotator CharacterRotation = GetActorRotation();
+
+		
+		MuzzleOffset.Set(100.0f, 0.0f, 50.0f);		
+		FVector MuzzleLocation = CharacterLocation + FTransform(CharacterRotation).TransformVector(MuzzleOffset);
+		FRotator MuzzleRotation = CharacterRotation;		
+		MuzzleRotation.Pitch += 10.0f;
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = GetInstigator();
+
+			
+			AHDBowProjectile* Projectile = World->SpawnActor<AHDBowProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+			if (Projectile)
+			{
+				FVector LaunchDirection = MuzzleRotation.Vector();
+				Projectile->FireInDirection(LaunchDirection);
+			}
+		}
+	}
+}
+void AHDPlayerCharacter::Attack(const FInputActionValue& value)
+{
+	if (!Controller) return;
+
+	bool bIsPressed = value.Get<bool>();
+
+	if(bIsPressed)
+	{
+		if (AttackMontage)
+		{
+			PlayAnimMontage(AttackMontage);
+		}
+		Fire();
+	}
+}
+
+float AHDPlayerCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if (ActualDamage <= 0.0f)
+	{
+		return 0.0f;
+	}
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (AnimInstance && TakeDamageMontage)
+	{
+		AnimInstance->Montage_Play(TakeDamageMontage);
+	}
+
+	HP = FMath::Clamp(HP - ActualDamage, 0.0f, MaxHP);
+	UE_LOG(LogTemp, Warning, TEXT("Hit damage: %f / %f"), HP, MaxHP);
+
+
+	return ActualDamage;
 }
--- Source/Ch03_ShooterGame/Public/HDPlayerCharacter.h
+++ Source/Ch03_ShooterGame/Public/HDPlayerCharacter.h
@@ -25,6 +25,8 @@ public:
 	UCameraComponent* CameraComp;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
 	class UAnimMontage* DashMontage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	class UAnimMontage* AttackMontage;
 
 	float HP;
 	float MaxHP;
@@ -38,7 +40,25 @@ protected:
 	void Move(const FInputActionValue& value);
 	UFUNCTION()
 	void Dash(const FInputActionValue& value);
+	UFUNCTION()
+	void Attack(const FInputActionValue& value);
+	UPROPERTY(EditDefaultsOnly, Category = "AI Animation")
+	UAnimMontage* TakeDamageMontage;
+	
 	bool bCanDash = true;
 	FTimerHandle DashCooldownTimerHandle;
 	void ResetDash();
+
+	UPROPERTY(EditAnywhere, Category = Projectile)
+	TSubclassOf<class AHDBowProjectile> ProjectileClass;
+
+	// 발사체를 발사하는 함수입니다.
+	UFUNCTION()
+	void Fire();
+
+	// 카메라 위치로부터의 총구 오프셋입니다.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	FVector MuzzleOffset;
+
+	float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
 };
